73224dceff08ed2660639467cea3477da23c9499
louloulfx <louloulfx1@hotmail.fr>
2018-12-06 15:27:22 +0100

    Liens entre la main page et la page profil

--- app/src/main/java/com/example/loulo/applicationmadrental/MainActivity.java
+++ app/src/main/java/com/example/loulo/applicationmadrental/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.loulo.applicationmadrental;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         TextView tx = (TextView)findViewById(R.id.Main_Title);
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/RacingSansOne-Regular.ttf");
-        tx.setTypeface(custom_font);
+        tx.setTypeface(custom_font);*/
+
+    }
+
+
+    public void ProfileActivity (View View) {
+        startActivity(new Intent(this, ProfileActivity.class));
     }
 
 }
