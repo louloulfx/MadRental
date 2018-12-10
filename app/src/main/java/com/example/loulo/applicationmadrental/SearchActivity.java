53fdf18b877a670c693b454134f46392c7116923
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2018-12-10 12:20:51 +0100

    Commit inutile

--- app/src/main/java/com/example/loulo/applicationmadrental/SearchActivity.java
+++ app/src/main/java/com/example/loulo/applicationmadrental/SearchActivity.java
@@ -2,21 +2,17 @@ package com.example.loulo.applicationmadrental;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
 
     public static TextView data;
-    public static ImageView dataView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         data = (TextView) findViewById(R.id.text_view_result);
-        /* dataView = (ImageView) findViewById(R.id.img_view_result); */
-        
         data process = new data();
         process.execute();
 
