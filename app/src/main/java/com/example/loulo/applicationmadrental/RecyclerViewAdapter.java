b38d7bce467a12dce178aed516048bd58ee4405c
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2018-12-28 17:02:53 +0100

    Reservation Page 1/2 done

--- app/src/main/java/com/example/loulo/applicationmadrental/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/loulo/applicationmadrental/RecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.example.loulo.applicationmadrental;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -34,8 +36,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         View view;
         LayoutInflater inflater = LayoutInflater.from(mContext);
         view = inflater.inflate(R.layout.vehicule_row_item, parent, false);
+        final MyViewHolder viewHolder = new MyViewHolder(view);
 
-        return new MyViewHolder(view);
+        viewHolder.view_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(mContext, Res1Activity.class);
+                i.putExtra("vehicule_name",mData.get(viewHolder.getAdapterPosition()).getNom());
+                i.putExtra("vehicule_price",mData.get(viewHolder.getAdapterPosition()).getPrixjournalierbase());
+                i.putExtra("vehicule_category",mData.get(viewHolder.getAdapterPosition()).getCategorieco2());
+                i.putExtra("vehicule_image",mData.get(viewHolder.getAdapterPosition()).getImage());
+
+                mContext.startActivity(i);
+
+            }
+        });
+
+        return viewHolder;
     }
 
     @Override
@@ -63,10 +81,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         TextView tv_categorieco2;
         TextView tv_promotion;
         ImageView img_image;
+        LinearLayout view_container;
 
         public MyViewHolder(View itemView) {
             super(itemView);
 
+            view_container = itemView.findViewById(R.id.container);
             tv_nom = itemView.findViewById(R.id.vehicule_name);
             tv_prixjournalierbase = itemView.findViewById(R.id.vehicule_price);
             tv_categorieco2 = itemView.findViewById(R.id.vehicule_category);
--- app/src/main/java/com/example/loulo/applicationmadrental/Res1Activity.java
+++ app/src/main/java/com/example/loulo/applicationmadrental/Res1Activity.java
@@ -2,6 +2,12 @@ package com.example.loulo.applicationmadrental;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.loulo.applicationmadrental.R;
 
 public class Res1Activity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class Res1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res1);
+
+        String nom = getIntent().getExtras().getString("vehicule_name");
+        String image = getIntent().getExtras().getString("vehicule_image");
+        int prixjournalierbase = getIntent().getExtras().getInt("vehicule_price");
+        String categorieco2 = getIntent().getExtras().getString("vehicule_category");
+
+
+        TextView tv_nom = findViewById(R.id.r_vehicule_name);
+        ImageView iv_image = findViewById(R.id.r_vehicule_image);
+        TextView tv_prix = findViewById(R.id.r_vehicule_price);
+        TextView tv_categorie = findViewById(R.id.r_vehicule_category);
+
+
+        RequestOptions requestOptions = new RequestOptions().centerCrop().placeholder(R.drawable.loading_shape).error(R.drawable.loading_shape);
+
+        tv_nom.setText(nom);
+        Glide.with(this).load(image).apply(requestOptions).into(iv_image);
+        tv_prix.setText(prixjournalierbase+" € / jour");
+        tv_categorie.setText(categorieco2);
+
     }
+
 }
